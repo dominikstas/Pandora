5c90fc26ef86f3db2ce650a816a54d1f5098dca5
Dominik Stasiak <d.stasiak436@gmail.com>
2024-04-24 09:34:26 +0200

    add: skipping cutscenes (space)

--- scripts/menu/intro.py
+++ scripts/menu/intro.py
@@ -1,54 +1,50 @@
 import pygame
 import time
 import os
+import sys
 
 # Create a full path to the fonts folder -> bauhaus font (for all the tekst)
 FONT_FOLDER = os.path.join("assets", "fonts", "bauhaus")
 
 def intro(screen, SCREEN_WIDTH, SCREEN_HEIGHT):
-    """
-    intro function displaying an animation.
-
-    Args:
-        screen (pygame.Surface): The surface representing the game window.
-        SCREEN_WIDTH (int): Width of the game window.
-        SCREEN_HEIGHT (int): Height of the game window.
-    """
-
     # Set up font
     font = pygame.font.Font(os.path.join(FONT_FOLDER, "bauhaus.ttf"), int(SCREEN_HEIGHT * 0.06))
 
     # Clear the screen
     screen.fill((0, 0, 0))
     pygame.display.flip()
-
-    # Display the animation
-    animation_text = font.render("Welcome", True, (255, 255, 255))
-    animation_rect = animation_text.get_rect(center=(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2))
-    screen.blit(animation_text, animation_rect)
-    pygame.display.flip()
-
-    # Wait for a few seconds
-    pygame.time.wait(1000)
     
-    screen.fill((0, 0, 0))  # Clear the screen
-    animation_text = font.render("to", True, (255, 255, 255))
-    animation_rect = animation_text.get_rect(center=(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2))
-    screen.blit(animation_text, animation_rect)
-    pygame.display.flip()
+    # Set up animation texts
+    animation_texts = [" ", "Welcome", "to", "Pandora"]
+    animation_delays = [1000, 1000, 1000, 2000]  # Delays in milliseconds between each text
 
-    # Wait for a few seconds
-    pygame.time.wait(1000)
+    intro_done = False
+    current_text_index = 0
+    start_time = pygame.time.get_ticks()
 
-    # Display "Pandora"
-    screen.fill((0, 0, 0)) 
-    pandora_text = font.render("Pandora", True, (20, 80, 20)) #Dark green
-    pandora_text = pygame.transform.scale(pandora_text, (pandora_text.get_width() * 2, pandora_text.get_height() * 2))
-    pandora_rect = pandora_text.get_rect(center=(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2))
-    screen.blit(pandora_text, pandora_rect)
-    pygame.display.flip()
+    while not intro_done:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    intro_done = True
 
-
-    # Wait for a few seconds
-    pygame.time.wait(2000)
+        current_time = pygame.time.get_ticks()
+        if current_time - start_time >= animation_delays[current_text_index]:
+            current_text_index += 1
+            if current_text_index >= len(animation_texts):
+                intro_done = True
+            else:
+                start_time = current_time
+                screen.fill((0, 0, 0))
+                if animation_texts[current_text_index] == "Pandora":
+                    animation_text = font.render(animation_texts[current_text_index], True, (20, 80, 20))  # Dark green
+                    animation_text = pygame.transform.scale(animation_text, (animation_text.get_width() * 2, animation_text.get_height() * 2))
+                else:
+                    animation_text = font.render(animation_texts[current_text_index], True, (255, 255, 255))
+                animation_rect = animation_text.get_rect(center=(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2))
+                screen.blit(animation_text, animation_rect)
+                pygame.display.flip()
 
--- scripts/menu/new_game.py
+++ scripts/menu/new_game.py
@@ -1,21 +1,13 @@
 import pygame
 import time
 import os
+import sys
 import scripts.inter.interface as interface
 
 # Create a full path to the fonts folder -> bauhaus font (for all the tekst)
 FONT_FOLDER = os.path.join("assets", "fonts", "bauhaus")
 
 def new_game(screen, SCREEN_WIDTH, SCREEN_HEIGHT):
-    """
-    New game screen function displaying an animation.
-
-    Args:
-        screen (pygame.Surface): The surface representing the game window.
-        SCREEN_WIDTH (int): Width of the game window.
-        SCREEN_HEIGHT (int): Height of the game window.
-    """
-
     # Set up font
     font = pygame.font.Font(os.path.join(FONT_FOLDER, "bauhaus.ttf"), int(SCREEN_HEIGHT * 0.06))
 
@@ -24,47 +16,41 @@ def new_game(screen, SCREEN_WIDTH, SCREEN_HEIGHT):
     pygame.display.flip()
 
     # Display the animation
-    animation_text = font.render("Chapter 1 - Something", True, (20, 80, 20)) #Dark green    
-    animation_rect = animation_text.get_rect(center=(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2))
-    screen.blit(animation_text, animation_rect)
-    pygame.display.flip()
+    animation_texts = [
+        " ",
+        "Chapter 1 - Something",
+        "Here you can see",
+        "i mean, not right now",
+        "cool intro",
+        "and now you can play"
+    ]
+    animation_delays = [1000, 3000, 2000, 2000, 2000, 2000]  # Delays in milliseconds between each text
 
-    # Wait for a few seconds
-    pygame.time.wait(3000)
-    
-    screen.fill((0, 0, 0))  # Clear the screen
-    animation_text = font.render("Here you can see", True, (255, 255, 255))
-    animation_rect = animation_text.get_rect(center=(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2))
-    screen.blit(animation_text, animation_rect)
-    pygame.display.flip()
-    
-    # Wait for a few seconds
-    pygame.time.wait(2000)
-    
-    screen.fill((0, 0, 0))  # Clear the screen
-    animation_text = font.render("i mean, not right now", True, (255, 255, 255))
-    animation_rect = animation_text.get_rect(center=(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2))
-    screen.blit(animation_text, animation_rect)
-    pygame.display.flip()
+    intro_done = False
+    current_text_index = 0
+    start_time = pygame.time.get_ticks()
 
-    # Wait for a few seconds
-    pygame.time.wait(2000)
+    while not intro_done:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
+            elif event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_SPACE:
+                    intro_done = True
 
-    screen.fill((0, 0, 0))  # Clear the screen
-    animation_text = font.render("cool intro", True, (255, 255, 255))
-    animation_rect = animation_text.get_rect(center=(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2))
-    screen.blit(animation_text, animation_rect)
-    pygame.display.flip()
-    
-    pygame.time.wait(2000)
-    
-    screen.fill((0, 0, 0))  # Clear the screen
-    animation_text = font.render("and now you can play", True, (255, 255, 255))
-    animation_rect = animation_text.get_rect(center=(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2))
-    screen.blit(animation_text, animation_rect)
-    pygame.display.flip()
+        current_time = pygame.time.get_ticks()
+        if current_time - start_time >= animation_delays[current_text_index]:
+            current_text_index += 1
+            if current_text_index >= len(animation_texts):
+                intro_done = True
+            else:
+                start_time = current_time
+                screen.fill((0, 0, 0))
+                animation_text = font.render(animation_texts[current_text_index], True, (255, 255, 255))
+                animation_rect = animation_text.get_rect(center=(SCREEN_WIDTH // 2, SCREEN_HEIGHT // 2))
+                screen.blit(animation_text, animation_rect)
+                pygame.display.flip()
 
-    # Wait for a few seconds
-    pygame.time.wait(2000)
-
+    # Launch the interface
     interface.interface(screen, SCREEN_WIDTH, SCREEN_HEIGHT)
